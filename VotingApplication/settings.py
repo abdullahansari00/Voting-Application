b7646ffee4cd89d7800dbe0eccfee7deb709d60e
abdullahansari00 <aabdulam00@gmail.com>
2020-08-26 00:26:35 +0530

    to sqlite

--- VotingApplication/settings.py
+++ VotingApplication/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'VotingApplication.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'votingapplication',
@@ -84,16 +84,15 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306'
     }
-}
+}'''
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
